a430a0de75d4676718aa66c3f071d5be624d78cb
ian mongi <ianmongi45@gmail.com>
2023-03-24 01:35:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * main - prints text into standard error
+ * Return: Always 1 (Success
+ */
 int main(void)
 {
-    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 0;
-
-    while (msg[len])
-        len++;
-
-    len = write(2, msg, len);
-
-    return (len == -1 ? 1 : 0);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
 
