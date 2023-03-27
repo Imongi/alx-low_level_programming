4e2199801b80ce9f20960ff9299bb79981fde7db
ian mongi <ianmongi45@gmail.com>
2023-03-27 03:40:32 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (succes)
+ *
+ * main - tells positive or negative
+ */
+
+int main(void)
+{
+	int n;srand(time(0));
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* My code starts here */
+	if (n > 0)
+		printf("%d is positive\n" ,n);
+	if else (n == 0)
+		printf("%d is zero\n" ,n);
+	else 
+		printf("%d is negative\n" ,n);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,9 @@
+variables with the while loop
+variables using printf
+What is the ASCII character set
+How to use the if, if ... else statements
+How to use comments
+How to declare variables of types char, int, unsigned int
+How to assign values to variables
+How to print the values of variables of type char, int, unsigned int with printf
+How to use the while loop
