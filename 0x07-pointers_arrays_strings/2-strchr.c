559551700f2b7df6b286ccfaa19d68afb26744c2
ian mongi <ianmongi45@gmail.com>
2023-04-03 20:11:15 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _strchr - Entry point
  * @s: input
@@ -8,14 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-    int i = 0;
+	int i = 0;
 
-    for (; s[i] >= '\0'; i++)
-    {
-        if (s[i] == c)
-            return (&s[i]);
-    }
-
-    return (0);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
-
